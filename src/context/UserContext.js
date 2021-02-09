b7fee0a9d98903e94a1738c164755df024600d04
TIAGO-DELL <tiagolbassani@gmail.com>
2021-02-09 20:31:42 -0300

    Test

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -53,7 +53,7 @@ export const UserProvider = ({ children }) => {
   async function signOut() {
     console.log('Sign Out from Context');
     setJWT(null);
-    removeCookie('sid', { path: '/', domain: 'makeup-yourmind.herokuapp.com' });
+    removeCookie('sid', { path: '/', domain: 'https://makeup-yourmind.herokuapp.com/' });
     //await authService.signOutService(jwt);
   }
 
