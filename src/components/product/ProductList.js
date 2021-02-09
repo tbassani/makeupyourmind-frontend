6c42491abedd2f6e820e05c6278b70028cdac20f
TIAGO-DELL <tiagolbassani@gmail.com>
2021-02-09 16:02:53 -0300

    Inifinite scroll improved

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -3,6 +3,7 @@ import React, { useCallback, useRef, useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Spinner from 'react-bootstrap/Spinner';
 
 import { useApp } from '../../context/AppContext.js';
 
@@ -34,47 +35,26 @@ const ProductList = (props) => {
 
   const renderCard = (card, index) => {
     if (card.id) {
-      if (!loadingProducts) {
-        if (products.length === index + 1 && hasMore) {
-          return (
-            <ProductCard
-              prodRef={lastProductRef}
-              key={card.id}
-              img={card.img_src}
-              name={card.name}
-              maker={card.maker}
-              price={card.price}
-              img_ref={card.ref}
-              isSigned={props.isSigned}
-              history={props.history}
-              product_id={card.id}
-              rating={
-                card.ratingAmount && parseInt(card.ratingAmount) !== 0
-                  ? card.totalRating / parseInt(card.ratingAmount)
-                  : 0
-              }
-            />
-          );
-        } else {
-          return (
-            <ProductCard
-              key={card.id}
-              img={card.img_src}
-              name={card.name}
-              maker={card.maker}
-              price={card.price}
-              img_ref={card.ref}
-              isSigned={props.isSigned}
-              history={props.history}
-              product_id={card.id}
-              rating={
-                card.ratingAmount && parseInt(card.ratingAmount) !== 0
-                  ? card.totalRating / parseInt(card.ratingAmount)
-                  : 0
-              }
-            />
-          );
-        }
+      if (products.length === index + 1 && hasMore) {
+        return (
+          <ProductCard
+            prodRef={lastProductRef}
+            key={card.id}
+            img={card.img_src}
+            name={card.name}
+            maker={card.maker}
+            price={card.price}
+            img_ref={card.ref}
+            isSigned={props.isSigned}
+            history={props.history}
+            product_id={card.id}
+            rating={
+              card.ratingAmount && parseInt(card.ratingAmount) !== 0
+                ? card.totalRating / parseInt(card.ratingAmount)
+                : 0
+            }
+          />
+        );
       } else {
         return (
           <ProductCard
@@ -84,15 +64,14 @@ const ProductList = (props) => {
             maker={card.maker}
             price={card.price}
             img_ref={card.ref}
-            isSpinner={true}
             isSigned={props.isSigned}
             history={props.history}
+            product_id={card.id}
             rating={
               card.ratingAmount && parseInt(card.ratingAmount) !== 0
                 ? card.totalRating / parseInt(card.ratingAmount)
                 : 0
             }
-            product_id={card.id}
           />
         );
       }
@@ -101,6 +80,7 @@ const ProductList = (props) => {
   return (
     <Col style={{ padding: '7%' }}>
       <Row>{products.map(renderCard)}</Row>
+      {loadingProducts ? <Spinner animation="border" variant="primary" /> : <div></div>}
     </Col>
   );
 };
